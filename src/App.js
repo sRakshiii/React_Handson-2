444d49c1043a5288ab8e29b68c8cdd698767f34a
sRakshiii <86668172+sRakshiii@users.noreply.github.com>
2023-07-18 20:00:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
-import './App.css';
-import { Component } from "react";
-class App extends Component{
-    
-    state={
-            name:"",
-            dpt:"",
-            rating:"",
-            data:[]
-    }
+import React from 'react'
+import Form from './Form'
 
-    handlechange = (event)=>{
-      this.setState({[event.target.name]:event.target.value})
-    }
-
-      
-    handleClick=()=>{
-      const obj={
-            name:this.state.name,
-            dpt:this.state.dpt,
-            rating:this.state.rating
-          }
-      this.state.data.push(obj);
-      this.setState({data:this.state.data, name: "", dpt: "", rating:""})
-    }
-    render(){
-      return(
-          <div className="App">
-          <h2>EMPLOYEE FEEDBACK FORM</h2>
-          <br></br>
-          <label>Name :  
-          <input type="text" name="name" onChange={this.handlechange}  value={this.state.name} className="inputBox"/>
-          </label>
-          <br></br>
-          <br></br>
-          <label>Department :  
-          <input type="text" name="dpt" onChange={this.handlechange}  value={this.state.dpt} className="inputBox"/>
-          </label>
-          <br></br>
-          <br></br>
-          <label>Rating :  
-          <input type="text" name="rating" onChange={this.handlechange}  value={this.state.rating} className="inputBox"/>
-          </label>
-          <br></br>
-          <br></br>
-          <button onClick={this.handleClick}>Submit</button>
-          <br></br>
-          <br></br>
-          <div className="mainBox">
-          {this.state.data.map((item)=>{
-            return(
-              <h4 className="box">Name : {item.name} | Department : {item.dpt} | Rating : {item.rating}</h4>
-            )
-          })}
-          </div>
-          </div>
-      )
-    }
+function App() {
+  return (
+    <>
+    <Form/>
+    </>
+  )
 }
-export default App;
+
+export default App
